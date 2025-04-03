d04ae7516dfc851e7c69d4642f04191814188920
gaspiii <osoriogasparr@gmail.com>
2025-04-03 17:56:44 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+  },
+});
