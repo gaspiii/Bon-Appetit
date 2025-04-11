808ce5435c79d1f15bf60d7a2302449fbab18953
gaspiii <osoriogasparr@gmail.com>
2025-04-10 23:40:23 -0400

    aa

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Contact from "./pages/Contact.jsx";
 import Services from "./pages/Services.jsx";
 import AboutUs from "./pages/About";
 
