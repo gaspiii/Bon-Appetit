53ba4a1d3ef31e2890dc360a2eacbbbdcab5b258
gaspiii <osoriogasparr@gmail.com>
2025-04-10 23:13:46 -0400

    d

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    host: true
-  },
-  // 👇 Esto es lo importante para Vercel
   base: '/',
-});
+  plugins: [react()]
+})
